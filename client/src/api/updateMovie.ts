f90833bf8a4e53e67db6290e28c5c48d3981e038
wrightdj99 <wrightdj99@gmail.com>
2025-07-24 18:29:11 -0500

    Finished bug on EditMovie

--- client/src/api/updateMovie.ts
+++ client/src/api/updateMovie.ts
@@ -4,5 +4,5 @@ import type { MovieInput } from "./createMovie";
 const BASE_URL = "https://localhost:5001/api/movies/"
 
 export async function updateMovie(id: string, movie: MovieInput) {
-    await axios.put(`${BASE_URL}${id}`, movie);
+    await axios.put(`${BASE_URL}`, movie);
 }
