8d5a4a1975a2f8975c57366ffdbae1a9d3cd8e9a
wrightdj99 <wrightdj99@gmail.com>
2025-07-28 17:25:13 -0500

    Priming for deployment

--- API/Program.cs
+++ API/Program.cs
@@ -21,6 +21,9 @@ builder.Services.AddMediatR(x =>
 builder.Services.AddAutoMapper(typeof(MappingProfiles).Assembly);
 var app = builder.Build();
 
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -34,9 +37,11 @@ app.UseAuthorization();
 // Configure the HTTP request pipeline.
 
 app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
-.WithOrigins("http://localhost:5173", "https://localhost:5173"));
+.WithOrigins("http://localhost:5173", "https://localhost:5173", "https://movies-to-watch-app-danwright-b3d5gaagdpfmcuer.centralus-01.azurewebsites.net"));
 
 app.MapControllers();
+
+app.MapFallbackToFile("index.html");
 
 using var scope = app.Services.CreateScope();
 var services = scope.ServiceProvider;
